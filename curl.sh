f45ebbafe97d5293feae4e24c69c9ad65d6ccf9e
Jerry Gamblin <jerry.gamblin@gmail.com>
2016-10-04 20:11:59 -0500

    Add files via upload

---
+++ curl.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+target="http://jgamblin.com"
+
+echo
+echo -e "Curling $target to take WAF detection"
+echo 
+
+while true
+do 
+UserAgent=`gshuf -n 1 ua.txt`
+	STARTTIME=$(date +%s)
+	for run in {1..10}
+	do 
+	curl --compressed -A "$UserAgent" -sL -w "%{http_code} %{url_effective}\\n" $target -o /dev/null
+	done
+	ENDTIME=$(date +%s)
+	echo 
+	echo -e "It took $(($ENDTIME - $STARTTIME)) seconds to complete 10 curls using:"
+	echo -e "$UserAgent"
+	echo
+done
